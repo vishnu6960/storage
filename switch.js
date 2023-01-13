0c0a9bb0e0e0b945032910e2245e551a2dfcea0c
vishnu <kannasri113@gmail.com>
2023-01-13 22:20:51 +0530

    fordemos

---
+++ switch.js
@@ -0,0 +1,11 @@
+var a = new Date()
+switch(a.getDay()){
+    case 0 :
+        console.log("sunday")
+        break;
+    case 4 :
+        console.log("saturday")
+        break;
+    default:
+        console.log("not a day")
+}
