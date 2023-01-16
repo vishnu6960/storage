2f68bdb6174e9c2c56b3c60efbd25a8fbb766420
vishnu <kannasri113@gmail.com>
2023-01-16 09:09:00 +0530

    day

--- switch.js
+++ switch.js
@@ -3,13 +3,22 @@ switch(a.getDay()){
     case 0 :
         console.log("sunday")
         break;
+    case 1 :
+        console.log("monday")
+        break;
+    case 2 :
+        console.log("tuesday")
+        break;
     case 3 :
-        console.log("thursday")
+        console.log("wednesday")
         break;
     case 4 :
-        console.log("friday")
+        console.log("thursday")
         break;
     case 5 :
+        console.log("friday")
+        break;
+    case 6 :
         console.log("saturday")
         break;
     default:
